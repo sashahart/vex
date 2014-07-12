7c8e5ef085bfbc5467fdf85395309d551376e7b1
Sasha Hart <s@sashahart.net>
2014-07-11 19:37:43 -0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as inp:
 
 setup(
     name="vex",
-    version="0.0.8",
+    version="0.0.9",
     author="Sasha Hart",
     author_email="s@sashahart.net",
     url="http://github.com/sashahart/vex",
